8faff8ef873f34a2648721b34c82e673b864e0c0
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-03-02 18:13:34 -0300

    app login

--- www/js/LoginView.js
+++ www/js/LoginView.js
@@ -1,5 +1,7 @@
 var LoginView = function (template) {
 
+    self = this;
+
     this.initialize = function () {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
@@ -7,35 +9,12 @@ var LoginView = function (template) {
         this.el.on('click', '#loginButton', this.login);
         this.el.on('click', '#registerButton', this.register);
 
-        /*if(window.localStorage["email"] != undefined && window.localStorage["password"] != undefined) {*/
-//            var email = window.localStorage["email"];
-  //          var password = window.localStorage["password"];
+        if(window.localStorage['email'] != undefined && window.localStorage['password'] != undefined) {
+            var email = window.localStorage['email'];
+            var password = window.localStorage['password'];
 
-            var email = "email";
-            var password = "password";
-
-
-            var jqxhr = $.ajax({
-                crossDomain: true,
-                data: {
-                    email : email,
-                    password : password,
-                },
-                type: "POST",
-                url: "http://partiuai.com.br/login/",
-            }).done(function(data, textStatus, jqXHR) {
-                alert("Done! data: " + data + " textStatus: " + textStatus + " jqXHR: " + jqXHR);
-
-                window.localStorage["email"] = email;
-                window.localStorage["password"] = password;
-
-                window.location.hash = 'home';
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-                //alert("Fail! jqXHR: " + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
-
-                window.location.hash = 'home';
-            });
-        /*}*/
+            self.auth(email, password);
+        }
     };
 
     this.render = function() {
@@ -70,25 +49,29 @@ var LoginView = function (template) {
             var email = emailInput.val();
             var password = passwordInput.val();
 
-            var jqxhr = $.ajax({
-                crossDomain: true,
-                data: {
-                    email : email,
-                    password : password,
-                },
-                type: "POST",
-                url: "http://partiuai.com.br/login/",
-            }).done(function(data, textStatus, jqXHR) {
-                alert("Done! data: " + data + " textStatus: " + textStatus + " jqXHR: " + jqXHR);
-
-                window.localStorage["email"] = email;
-                window.localStorage["password"] = password;
-
-                window.location.hash = 'home';
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-                alert("Fail! jqXHR: " + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
-            });
+            self.auth(email, password);
         }
+    }
+
+    this.auth = function (email, password) {
+        var jqxhr = $.ajax({
+            crossDomain: true,
+            data: {
+                email : email,
+                password : password,
+            },
+            type: 'POST',
+            url: 'http://partiuai.com.br/login/',
+        }).done(function(data, textStatus, jqXHR) {
+            alert("Done! data: " + data + " textStatus: " + textStatus + " jqXHR: " + jqXHR);
+
+            window.localStorage['email'] = email;
+            window.localStorage['password'] = password;
+
+            window.location.hash = 'home';
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert("Fail! jqXHR: " + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
+        });
     }
 
     this.register = function () {
