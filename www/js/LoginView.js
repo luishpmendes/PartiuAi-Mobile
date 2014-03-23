3dd7be6f23620faa8776d1aef93c9240770cd20f
Luis H. P. Mendes <luishpmendes@gmail.com>
2014-03-23 17:38:12 -0300

    login

--- www/js/LoginView.js
+++ www/js/LoginView.js
@@ -74,7 +74,7 @@ var LoginView = function (template) {
                         $.ajax({
                             crossDomain: true,
                             type: 'GET',
-                            url: 'http://www.partiuai.com.br/register-by-access-token/facebook/accessToken='+response.authResponse.accessToken,
+                            url: 'http://www.partiuai.com.br/register-by-access-token/facebook/?accessToken='+response.authResponse.accessToken,
                         }).done(function(data, textStatus, jqXHR) {
                             console.log("FB.login done");
                             window.location.replace('main.html#home'); /* current page will NOT be saved in session history */
